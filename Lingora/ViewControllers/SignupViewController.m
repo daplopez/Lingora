5d26c5ea228d7dd5a907eff09d10117490a4c259
Daphne Lopez <daplopez@fb.com>
2022-07-07 16:52:25 -0700

    added picker views for both native and target languages on sign up screen

--- Lingora/ViewControllers/SignupViewController.m
+++ Lingora/ViewControllers/SignupViewController.m
@@ -9,7 +9,7 @@
 #import <Parse/Parse.h>
 #import "SceneDelegate.h"
 
-@interface SignupViewController ()
+@interface SignupViewController () <UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *emailField;
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
@@ -17,6 +17,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *nativeLanguageField;
 @property (weak, nonatomic) IBOutlet UITextField *targetLanguageField;
 @property (weak, nonatomic) IBOutlet UITextField *proficiencyLevelField;
+@property (strong, nonatomic) NSArray *languages;
+@property (weak, nonatomic) IBOutlet UIPickerView *nativeLanguagePickerView;
+@property (weak, nonatomic) IBOutlet UIPickerView *targetLanguagePickerView;
 
 @end
 
@@ -24,7 +27,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.nativeLanguagePickerView.delegate = self;
+    self.nativeLanguagePickerView.dataSource = self;
+    self.targetLanguagePickerView.delegate = self;
+    self.targetLanguagePickerView.dataSource = self;
+    
+    self.nativeLanguageField.delegate = self;
+    self.nativeLanguageField.inputView = self.nativeLanguagePickerView;
+    self.targetLanguageField.delegate = self;
+    self.targetLanguageField.inputView = self.targetLanguagePickerView;
+    
+    [self.nativeLanguagePickerView setHidden:YES];
+    [self.targetLanguagePickerView setHidden:YES];
+    
+    self.languages = [[NSArray alloc] initWithObjects:@"English", @"Spanish", nil];
+    
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    if ([textField isEqual:self.nativeLanguageField]) {
+        [self.nativeLanguagePickerView setHidden:NO];
+    } else {
+        [self.targetLanguagePickerView setHidden:NO];
+    }
+    return NO;
+}
+
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.languages.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.languages[row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if ([pickerView isEqual:self.nativeLanguagePickerView]) {
+        self.nativeLanguageField.text = self.languages[row];
+        [self.nativeLanguagePickerView setHidden:YES];
+    } else {
+        self.targetLanguageField.text = self.languages[row];
+        [self.targetLanguagePickerView setHidden:YES];
+    }
 }
 
 - (IBAction)didTapSignup:(id)sender {
