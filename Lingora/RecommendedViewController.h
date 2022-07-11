b056d34acee2bb90ef5132e1f57036614271df75
Daphne Lopez <daplopez@fb.com>
2022-07-11 13:21:50 -0700

    setting up RecommendedVC

---
+++ Lingora/RecommendedViewController.h
@@ -0,0 +1,16 @@
+//
+//  RecommendedViewController.h
+//  Lingora
+//
+//  Created by Daphne Lopez on 7/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RecommendedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lingora/RecommendedViewController.m
@@ -0,0 +1,44 @@
+//
+//  RecommendedViewController.m
+//  Lingora
+//
+//  Created by Daphne Lopez on 7/11/22.
+//
+
+#import "RecommendedViewController.h"
+#import "Parse/PFImageView.h"
+#import "Parse/Parse.h"
+
+@interface RecommendedViewController ()
+
+@property (weak, nonatomic) IBOutlet PFImageView *profilePicture;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nativeLanguageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *targetLanguageLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation RecommendedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self getUserData];
+    [self.tableView reloadData];
+    
+}
+
+- (void)getUserData {
+    PFUser *curUser = [PFUser currentUser];
+    self.nameLabel.text = curUser[@"fullName"];
+    self.nativeLanguageLabel.text = curUser[@"nativeLanguage"];
+    self.targetLanguageLabel.text = curUser[@"targetLanguage"];
+    if (curUser[@"image"] != nil) {
+        self.profilePicture.file = curUser[@"user"];
+        [self.profilePicture loadInBackground];
+    }
+}
+
+
+@end
