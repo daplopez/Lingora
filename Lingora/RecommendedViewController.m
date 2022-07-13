6bdde5cae46dfa62be77b72917acab54b37662dc
Daphne Lopez <daplopez@fb.com>
2022-07-13 11:19:28 -0700

    added manual segue from recommended screen to home feed

--- Lingora/RecommendedViewController.m
+++ Lingora/RecommendedViewController.m
@@ -8,6 +8,8 @@
 #import "RecommendedViewController.h"
 #import "Parse/PFImageView.h"
 #import "Parse/Parse.h"
+#import "HomeFeedViewController.h"
+#import "SceneDelegate.h"
 
 @interface RecommendedViewController ()
 
@@ -38,6 +40,12 @@
         self.profilePicture.file = curUser[@"image"];
         [self.profilePicture loadInBackground];
     }
+}
+
+- (IBAction)didTapPosts:(id)sender {
+    SceneDelegate *myDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    myDelegate.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"TabBar"];
 }
 
 
