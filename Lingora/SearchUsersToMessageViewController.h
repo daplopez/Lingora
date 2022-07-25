b677235f0706a41d73ad82c1f2149080831c07e1
Daphne Lopez <daplopez@fb.com>
2022-07-25 10:53:34 -0700

    added SearchUsersToMessageVC to implement messaging

---
+++ Lingora/SearchUsersToMessageViewController.h
@@ -0,0 +1,16 @@
+//
+//  SearchUsersToMessageViewController.h
+//  Lingora
+//
+//  Created by Daphne Lopez on 7/25/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchUsersToMessageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
