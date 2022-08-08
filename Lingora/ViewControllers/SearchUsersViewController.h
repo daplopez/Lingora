c6e339d543be6489f7f04cfb60a70309ceb68577
Daphne Lopez <daplopez@fb.com>
2022-08-08 14:44:33 -0700

    added a view controller to search for users from the home screen

---
+++ Lingora/ViewControllers/SearchUsersViewController.h
@@ -0,0 +1,16 @@
+//
+//  SearchUsersViewController.h
+//  Lingora
+//
+//  Created by Daphne Lopez on 8/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchUsersViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lingora/ViewControllers/SearchUsersViewController.m
@@ -0,0 +1,31 @@
+//
+//  SearchUsersViewController.m
+//  Lingora
+//
+//  Created by Daphne Lopez on 8/8/22.
+//
+
+#import "SearchUsersViewController.h"
+
+@interface SearchUsersViewController ()
+
+@end
+
+@implementation SearchUsersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
