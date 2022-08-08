5ef7374e85b0e60a79e2e68f22f4ae8e6ac817e7
Daphne Lopez <daplopez@fb.com>
2022-08-08 16:03:38 -0700

    added a view controller to filter through users when searching from the home screen

---
+++ Lingora/ViewControllers/FilterUserSearchViewController.m
@@ -0,0 +1,43 @@
+//
+//  FilterUserSearchViewController.m
+//  Lingora
+//
+//  Created by Daphne Lopez on 8/8/22.
+//
+
+#import "FilterUserSearchViewController.h"
+
+@interface FilterUserSearchViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *locationField;
+@property (weak, nonatomic) IBOutlet UITextField *targetLanguageField;
+
+@end
+
+@implementation FilterUserSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapAddInterest:(id)sender {
+    
+}
+
+
+- (IBAction)didTapSearch:(id)sender {
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
