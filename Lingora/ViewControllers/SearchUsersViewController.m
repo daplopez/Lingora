16bf56defa65cedc6dc51a6075dfc68681208aa1
Daphne Lopez <daplopez@fb.com>
2022-08-08 19:07:33 -0700

    added query in searchUsersVC

--- Lingora/ViewControllers/SearchUsersViewController.m
+++ Lingora/ViewControllers/SearchUsersViewController.m
@@ -6,8 +6,13 @@
 //
 
 #import "SearchUsersViewController.h"
+#import "Parse/Parse.h"
+#import "UserSearchTableViewCell.h"
 
-@interface SearchUsersViewController () <FilterUserSearchDelegate>
+@interface SearchUsersViewController () <FilterUserSearchDelegate, UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) NSArray *users;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -15,15 +20,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setDelegates];
+    [self queryForUsers];
 }
 
+
+- (void)setDelegates {
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+}
+
+- (void)queryForUsers {
+    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
+    if (self.interests != nil) {
+        [query whereKey:@"interests" containsAllObjectsInArray:self.interests];
+    }
+    if (self.locationField != nil) {
+        double miles = [self.locationField doubleValue];
+        [query whereKey:@"location" nearGeoPoint:PFUser.currentUser[@"location"] withinMiles:miles];
+    }
+    if (self.targetLanguageField != nil) {
+        [query whereKey:@"targetLanguage" equalTo:self.targetLanguageField];
+    }
+    
+    // fetch data asynchronously
+    [query findObjectsInBackgroundWithBlock:^(NSArray *users, NSError *error) {
+        if (users != nil) {
+            NSLog(@"Successfully got users");
+            self.users = [[NSArray alloc] initWithArray:users];
+            NSLog(@"%d", self.users.count);
+            NSLog(@"%d", users.count);
+            NSLog(@"CHECK");
+            //[self setDataToFilterBy];
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+}
 
 - (void)sendFiltersBack:(NSString *)language withRange:(NSString *)miles interests:(NSArray *)interests {
     self.interests = interests;
     self.targetLanguageField = language;
     self.locationField = miles;
+    [self queryForUsers];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"%d", self.users.count);
+    NSLog(@"CHECK");
+    return self.users.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UserSearchTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"userSearchCell" forIndexPath:indexPath];
+    cell.profilePicture.file = self.users[indexPath.row][@"image"];
+    [cell.profilePicture loadInBackground];
+    //cell.nameLabel.text = self.filteredData[indexPath.row];
+    cell.nameLabel.text = self.users[indexPath.row][@"username"];
+    return cell;
+}
 
 #pragma mark - Navigation
 
